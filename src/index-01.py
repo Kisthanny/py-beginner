c01e801a14f258de1c29f4050b230ea6542a172f
KK <hongqi@tsintergy.com>
2025-03-23 11:38:23 +0800

    init

---
+++ src/index-01.py
@@ -0,0 +1,31 @@
+# 练习1
+print('Hello, 我的名字是 xxx！')
+print('今天是我学习Python的第1天。')
+
+# 练习2
+# 控制台打印开始计算
+print("开始计算")
+# 定义变量a，赋值为10
+a = 10
+# 定义变量b，赋值为5
+b = 5
+# 定义变量c，赋值为a和b的和
+c = a + b
+# 控制台打印c的值（15）
+print(c)
+
+# 练习3
+print("计算两个数字的和")
+a = 10
+# b = "5"
+b = 5
+c = a + b
+print("结果是：", c)
+
+# 练习4
+name = "小明"
+age = 10
+hobby = '炒饭'
+print(age)
+print(hobby)
+print(name, age, hobby, sep='---')
